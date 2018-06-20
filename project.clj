c0ef5631ac6201b99ccefc8c23ecd5cc8e4c0a77
Gary Fredericks <fredericksgary@gmail.com>
2018-06-20 18:59:58 -0500

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/dot-slash-2 "0.1.3"
+(defproject com.gfredericks/dot-slash-2 "0.1.4-SNAPSHOT"
   :description "Clojure library for easily creating proxy namespaces"
   :url "https://github.com/gfredericks/dot-slash-2"
   :license {:name "Eclipse Public License"
